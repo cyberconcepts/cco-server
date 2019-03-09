3a93a6fc071b1b2f6dcdb0b9263a3df300f16f27
Helmut Merz <helmutm@cy55.de>
2019-03-09 13:31:15 +0100

    new version with quite a lot of updated packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='main',
-      version='2.0',
+      version='2.1',
       description='main loops (bluebream) server application',
       long_description="""\
 """,
